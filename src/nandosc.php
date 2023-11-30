f531d18efac6fd57824eaa06867a86952271c8e3
Fernando Costa <fernandocostasud@gmail.com>
2023-11-30 20:01:22 -0300

    first commit

---
+++ src/nandosc.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nandosc\nandosc;
+
+final class nandosc
+{
+
+    /**
+     * @return void
+     */
+    public function render()
+    {
+        echo 'ola';
+    }
+
+}
